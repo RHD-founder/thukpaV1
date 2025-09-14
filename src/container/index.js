55847acfce84227af23c35ac511f2f7e681e0534
Hashim Ahmed <55762512+CodexHashim@users.noreply.github.com>
2025-09-14 20:32:21 +0530

    first commit

---
+++ src/container/index.js
@@ -0,0 +1,21 @@
+import AboutUs from "./AboutUs/AboutUs";
+import Chef from "./Chef/Chef";
+import Find from "./Findus/Find";
+import Footer from "./Footer/Footer";
+import Gallery from "./Gallery/Gallery";
+import Header from "./Header/Header";
+import Intro from "./Intro/Intro";
+import Laurels from "./Laurels/Laurels";
+import SpecialMenu from "./Menu/SpecialMenu";
+
+export {
+  AboutUs,
+  Chef,
+  Find,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+};
